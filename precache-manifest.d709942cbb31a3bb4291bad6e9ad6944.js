059a0475d3b5812a6e321c4f447f9622d3dbeb92
jollyrgr83 <jasongmangin@gmail.com>
2020-07-31 20:35:10 -0600

    Updates

---
+++ precache-manifest.d709942cbb31a3bb4291bad6e9ad6944.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a0931115e1bd12c8ba2bff74b42635",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "84eb991dc6f9f7fea0ca",
+    "url": "/portfolio-react/static/css/2.53573671.chunk.css"
+  },
+  {
+    "revision": "095771da956b7104c7f8",
+    "url": "/portfolio-react/static/css/main.f45591f0.chunk.css"
+  },
+  {
+    "revision": "84eb991dc6f9f7fea0ca",
+    "url": "/portfolio-react/static/js/2.1842c627.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-react/static/js/2.1842c627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "095771da956b7104c7f8",
+    "url": "/portfolio-react/static/js/main.d8beb2cb.chunk.js"
+  },
+  {
+    "revision": "feb6fec8f95565f3aa5d",
+    "url": "/portfolio-react/static/js/runtime-main.c94dc782.js"
+  },
+  {
+    "revision": "6379b2806192541a268337061fb3eafb",
+    "url": "/portfolio-react/static/media/a-crop.6379b280.png"
+  },
+  {
+    "revision": "b77576d2af94c143316490a3c61b6b6a",
+    "url": "/portfolio-react/static/media/bio2.b77576d2.jpg"
+  },
+  {
+    "revision": "34eef9c3d1ea1b53cb42c0b363895e08",
+    "url": "/portfolio-react/static/media/burger.34eef9c3.png"
+  },
+  {
+    "revision": "6ee0a498350d85399737d1ea6b919d39",
+    "url": "/portfolio-react/static/media/coding-quiz.6ee0a498.png"
+  },
+  {
+    "revision": "1647a9fa31349c816083e6302a794853",
+    "url": "/portfolio-react/static/media/day-planner.1647a9fa.png"
+  },
+  {
+    "revision": "cc8ed6f9ab834be848e520374d99c9c5",
+    "url": "/portfolio-react/static/media/event-scoring-database.cc8ed6f9.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "15afa445a98e536826f39f7c943a1dd2",
+    "url": "/portfolio-react/static/media/jason-mangin-resume.15afa445.pdf"
+  },
+  {
+    "revision": "647144a0f1a4bd5d9fbdb8d996b37e8e",
+    "url": "/portfolio-react/static/media/navy-crop.647144a0.png"
+  },
+  {
+    "revision": "0a1a8aa2f34f50f5a9d91cf0974d383c",
+    "url": "/portfolio-react/static/media/skyvi3w.0a1a8aa2.PNG"
+  },
+  {
+    "revision": "d5dccfa7505661459e1244a552550ed9",
+    "url": "/portfolio-react/static/media/story-lines.d5dccfa7.png"
+  },
+  {
+    "revision": "fbcfbd75dc512ce914a46787cee698d3",
+    "url": "/portfolio-react/static/media/weather-dashboard.fbcfbd75.png"
+  }
+]);
